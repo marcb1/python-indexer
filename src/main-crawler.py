dcf5a9089e04ac017e43bd2df0dc06ab324ae8bc
Marc B <marcbassil1@gmail.com>
2014-04-09 21:55:29 -0400

    updated usage

--- src/main-crawler.py
+++ src/main-crawler.py
@@ -9,7 +9,7 @@ import json
 def main(argv):
     domain = ''
     read_file = ''
-    usage = 'usage: something.py -u <http-url> (optional): -h (for help)  -w write_to_json_file -r read_from_json_file'
+    usage = 'usage: main-crawler.py -u <http-url> -r <file-to-read-from> (optional): -h (for help)'
     full_usage = 'something.py \n usage: something.py -u <http-url> (optional): -h (for help)  -w write_to_json_file -r read_from_json_file \n -u Please provide full http address here this field is required \n  \t Will print out the external javascript this page links to \n -r This is optional, provide a file name to read a list of addresses from'
     try:
         opts, args = getopt.getopt(argv,"hu:w:r:",["url=", "output-file=", "read="])
